475ee80ce62f63cee6996f8b64603afeeb9b5b4c
Denis-Kalinyik <99794679+Denis-Kalinyik@users.noreply.github.com>
2023-04-11 01:54:38 +0300

    Update countWords.js

--- countWords.js
+++ countWords.js
@@ -1,19 +1,17 @@
-/**
- * Given an input string, returns number of words contained in the string.
- *
- * In our xase, assume words are always separated by a single space.
- *
- * @param {string} string - The input string
- * @returns {number} The number of words in the string.
- */
-
 function countWords(string) {
-    // Put your code here
-}
-  
- 
-console.log('Running sanity checks for countWords:');
-
-console.log(countWords('Running sanity checks for countWords') == 5);
-
-// Add your own sanity checks here.
+    if (string.trim() === '') { // check if the string is empty
+    return 0;
+    }
+    return string.trim().split(/\s+/).length;
+    }
+    console.log(countWords('Running sanity checks for countWords') === 5);
+    console.log(countWords('') === 0);
+    console.log(countWords('one two') === 2);
+    console.log(countWords('1 2 3') === 3);
+    console.log(countWords('spaces between words') === 3);
+    console.log(countWords(' special characters !@#$%^&*()_+') === 3);
+    // Additional tests
+    console.log(countWords(' singleword ') === 1);
+    console.log(countWords('multiple spaces between words') === 4);
+    console.log(countWords(' 12345 ') === 1);
+    
