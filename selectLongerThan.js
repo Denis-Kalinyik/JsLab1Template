9a3739b432e9db5574f5d180cb7d0daaf7ff778e
Denis-Kalinyik <99794679+Denis-Kalinyik@users.noreply.github.com>
2023-04-11 02:10:13 +0300

    Update selectLongerThan.js

--- selectLongerThan.js
+++ selectLongerThan.js
@@ -1,27 +1,25 @@
-/**
- * Given an array of strings and a threshold length, returns a new array
- * consisting of only the strings with length strictly greater than the
- * given threshold.
- *
- * Return an empty array if no such strings exist.
- *
- * @example
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], -1); // => ['', 'aaa', 'bb', 'c', 'dddd']
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 0); // => ['aaa', 'bb', 'c', 'dddd']
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 1); // => ['aaa', 'bb', dddd']
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 2); // => ['aaa', dddd']
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 3); // => ['dddd']
- * selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 4); // => []
- *
- * @param {string[]} array - An array of strings
- * @param {number} threshold - A length threshold
- * @returns {string[]} An array of all strings in the input array with length
- *  strictly greater the given threshold
- */
 function selectLongerThan(array, threshold) {
-    // Put your code here
-}
-  
-console.log('Running sanity checks for selectLongerThan:');
-
-// Add your own sanity checks here.
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].length > threshold) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
+ 
+ 
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], -1));
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 0));
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 1));
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 2));
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 3));
+  console.log(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 4));
+ 
+ 
+  console.log(selectLongerThan([], 0)); // []
+  console.log(selectLongerThan([''], 0)); // ['']
+  console.log(selectLongerThan([''], 1)); // []
+  console.log(selectLongerThan(['a', 'ab', 'abc', 'abcd'], 2)); // ['abc', 'abcd']
+  console.log(selectLongerThan(['a', 'ab', 'abc', 'abcd'], 3)); // ['abcd']
+  console.log(selectLongerThan(['aaa', 'bb', 'c', 'ddd', 'eeeee'], 2)); // ['aaa', 'ddd', 'eeeee']
